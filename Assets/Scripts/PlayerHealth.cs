0771c1156117a9d9a08584ac3b6682ae841fe76d
SoggyFloorBiscut <120007739+SoggyFloorBiscut@users.noreply.github.com>
2023-01-11 12:14:51 +0100

    Player Basic movement & health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    int hp = 3;
+    SpriteRenderer sprite;
+    List<Color> colorchoose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        colorchoose = new List<Color>();
+        colorchoose.Add(Color.grey);
+        colorchoose.Add(Color.red);
+        colorchoose.Add(Color.black);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Danger")
+        {
+            print("aj");
+            hp =-1;
+            sprite.color = colorchoose[0 + 1];
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    bool jump = true;
+    float playerSpeed = 1000;
+    float playerJump = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddForce(Vector3.left * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddForce(Vector3.right * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && jump == true)
+        {
+            rb2d.AddForce(Vector3.up * playerJump);
+            jump = false;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Danger")
+        {
+
+        }
+        else
+        {
+            jump = true;
+        }
+    }
+}
