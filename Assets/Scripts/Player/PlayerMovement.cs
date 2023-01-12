94b5a3eb00e4c0d7ee982ed15b3482b4382854aa
SoggyFloorBiscut <120007739+SoggyFloorBiscut@users.noreply.github.com>
2023-01-12 14:21:41 +0100

    Player climb

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,8 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb2d;
     bool jump = true;
-    float playerSpeed = 1000;
-    float playerJump = 1000;
+    [SerializeField] bool climb = false;
+    [SerializeField] float playerSpeed = 800;
+    [SerializeField] float playerJump = 400;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKey(KeyCode.A))
         {
             rb2d.AddForce(Vector3.left * playerSpeed * Time.deltaTime);
@@ -30,18 +32,36 @@ public class PlayerMovement : MonoBehaviour
             rb2d.AddForce(Vector3.up * playerJump);
             jump = false;
         }
-
+        
+        if (climb == true && Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0.001f, 0);
+            rb2d.AddForce(Vector3.up * playerJump * 0.008f);
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Slope")
         {
-
+            
         }
         else
         {
             jump = true;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Climb")
+        {
+            climb = true;
+        }
+        else
+        {
+            climb = false;
+        }
+
     }
 
     public void PlayerDeath()
