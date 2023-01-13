d094715853b9785a6c2f6f3cf5de8725a887a674
SoggyFloorBiscut <120007739+SoggyFloorBiscut@users.noreply.github.com>
2023-01-13 11:38:10 +0100

    ChooseTriggerDoor

--- Assets/Scripts/DoorTriggerButton.cs
+++ Assets/Scripts/DoorTriggerButton.cs
@@ -6,16 +6,22 @@ using UnityEngine;
 public class DoorTriggerButton : MonoBehaviour
 {
     [SerializeField] private DoorSetActive door;
+    [SerializeField] GameObject chooseTrigger;
 
     private void Update()
     {
        if (Input.GetKeyDown(KeyCode.F))
-        {
+       {
             door.OpenDoor();
-        }
+       }
+       if (chooseTrigger.tag == "Active")
+       {
+            door.OpenDoor();
+       }
     }
     private void OnTriggerEnter2D(Collider2D collision2d)
     {
+        chooseTrigger.tag = "Inactive";
         door.CloseDoor();
         
     }
