9bfbd711cd051feaf91e39d7f76a65c2ab41226d
SoggyFloorBiscut <120007739+SoggyFloorBiscut@users.noreply.github.com>
2023-01-16 14:40:09 +0100

    Box activates button

--- Assets/Scripts/World/DoorTriggerButton.cs
+++ Assets/Scripts/World/DoorTriggerButton.cs
@@ -6,7 +6,15 @@ using UnityEngine;
 public class DoorTriggerButton : MonoBehaviour
 {
     [SerializeField] private DoorSetActive door;
-    [SerializeField] bool colorUse = true;
+
+    [SerializeField] bool colorUse1 = true;
+    [SerializeField] bool colorUse2 = true;
+    [SerializeField] bool colorUse3 = true;
+
+    bool active1;
+    bool active2;
+    bool active3;
+
     [SerializeField] Color chooseColor1;
     [SerializeField] Color chooseColor2;
     [SerializeField] Color chooseColor3;
@@ -22,12 +30,145 @@ public class DoorTriggerButton : MonoBehaviour
 
     private void Start()
     {
-        
+        if (chooseSprite1 == null)
+        {
+            bool colorUse1 = false;
+        }
+        if (chooseSprite2 == null)
+        {
+            bool colorUse2 = false;
+        }
+        if (chooseSprite3 == null)
+        {
+            bool colorUse3 = false;
+        }
 
     }
 
     private void Update()
     {
+        
+        if (colorUse1 == true)
+        {
+            if (chooseSprite1.color == chooseColor1)
+            {
+                active1 = true;
+            }
+            else
+            {
+                active1 = false;
+            }
+        }
+        else if (colorUse1 == false)
+        {
+            if (chooseTrigger1.tag == "Active")
+            {
+                active1 = true;
+            }
+            else
+            {
+                active1 = false;
+            }
+        }
+
+        if (colorUse2 == true)
+        {
+            if (chooseSprite2.color == chooseColor2)
+            {
+                active2 = true;
+            }
+            else
+            {
+                active2 = false;
+            }
+        }
+        else if (colorUse2 == false)
+        {
+            if (chooseTrigger2.tag == "Active")
+            {
+                active2 = true;
+            }
+            else
+            {
+                active2 = false;
+            }
+        }
+
+        if (colorUse3 == true)
+        {
+            if (chooseSprite3.color == chooseColor3)
+            {
+                active3 = true;
+            }
+            else
+            {
+                active3 = false;
+            }
+        }
+        else if (colorUse3 == false)
+        {
+            if (chooseTrigger3.tag == "Active")
+            {
+                active3 = true;
+            }
+            else
+            {
+                active3 = false;
+            }
+        }
+
+        if (active1 == true && active2 == true && active3 == true) 
+        {
+            door.OpenDoor();
+        }
+        else
+        {
+            door.CloseDoor();
+        }
+        /*
+
+        if (colorUse2 == true)
+        {
+            if (chooseSprite2.color == chooseColor2)
+            {
+                active2 = true;
+            }
+        }
+        if (colorUse3 == true)
+        {
+            if (chooseSprite3.color == chooseColor3)
+            {
+                active3 = true;
+            }
+        }
+        /*
+        if (chooseSprite1.color == chooseColor1)
+        {
+            chooseTrigger1.tag = "Active";
+        }
+        if (chooseSprite2.color == chooseColor2)
+        {
+            chooseTrigger2.tag = "Active";
+        }
+        if (chooseSprite3.color == chooseColor3)
+        {
+            chooseTrigger3.tag = "Active";
+        }
+        if (chooseTrigger1.tag == "Active" && chooseTrigger2.tag == "Active" && chooseTrigger3.tag == "Active")
+        {
+            door.OpenDoor();
+        }
+        else
+        {
+            door.CloseDoor();
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.F))
+       {
+            door.OpenDoor();
+       }
+        
         if (colorUse == false)
         {
             if (chooseTrigger1.tag == "Active" && chooseTrigger2.tag == "Active" && chooseTrigger3.tag == "Active")
@@ -50,11 +191,8 @@ public class DoorTriggerButton : MonoBehaviour
                 door.CloseDoor();
             }
         }
-       if (Input.GetKeyDown(KeyCode.F))
-       {
-            door.OpenDoor();
-       }
-       
+        */
+        
     }
     private void OnTriggerEnter2D(Collider2D collision2d)
     {
