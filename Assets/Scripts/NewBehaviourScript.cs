37ed38b93f5288c53586d1bab83c8f1d88305ad2
Miniitroll <leon.darby@elev.ga.lbs.se>
2023-01-16 10:37:37 +0100

    Fiende rörelse

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private float speed = 1.5f;
+    public bool rightmove = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rightmove == true)
+        {
+            transform.position += new Vector3(1.5f, 0, 0) * Time.deltaTime;
+            
+        }
+        else
+        {
+            transform.position += new Vector3(-1.5f, 0, 0) * Time.deltaTime;
+        }
+       
+       
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("noelfat");
+        if (rightmove == true)
+        {
+            rightmove = false;
+        }
+        else
+        {
+            rightmove = true;
+        }
+    }
+   
+}
