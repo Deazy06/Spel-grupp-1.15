c0be978ac578a473de679634ea10d77274a0608f
AlexandraSU22a <alexandra.langhed@elev.ga.lbs.se>
2023-01-17 12:32:22 +0100

    Push/pull box

--- Assets/PlayerPush.cs
+++ Assets/PlayerPush.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoxPull : MonoBehaviour
+public class PlayerPush : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
