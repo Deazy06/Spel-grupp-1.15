e12da925ddec12f6c3481dac7ede001ab8d8441f
WHTALI <william.hammarstrom@elev.ga.lbs.se>
2023-01-17 12:14:40 +0100

    ...

---
+++ Assets/Scripts/World/FullSceneToggle.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullSceneToggle : MonoBehaviour
+{
+    public void Fullscene(bool is_fullscene)
+    {
+        Screen.fullScreen = is_fullscene;
+
+        Debug.Log("fullscreen is activated");
+    }
+}
