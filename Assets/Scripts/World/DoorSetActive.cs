36de28870b7cdc16cbb9c37a19ed0f025ea02972
Miniitroll <leon.darby@elev.ga.lbs.se>
2023-01-19 13:13:53 +0100

    ljud grejer

--- Assets/Scripts/World/DoorSetActive.cs
+++ Assets/Scripts/World/DoorSetActive.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class DoorSetActive : MonoBehaviour
 {
+    public AudioSource door;
     public void OpenDoor()
     {
         gameObject.SetActive(false);
-       
+
+
+        door.Play();
     }
+
+
     public void CloseDoor()
     {
         gameObject.SetActive(true);
-       
+
     }
-    
 }
+    
+   
+    
