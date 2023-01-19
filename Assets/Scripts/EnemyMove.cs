acda7a8e477f21ff1839cdd3c3954c33643f46e8
Deazy06 <120008020+Deazy06@users.noreply.github.com>
2023-01-19 20:50:43 +0100

    EnemyMove Randomly Idle

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -7,29 +7,35 @@ public class EnemyMove : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     public bool rightmove = true;
     private Animator animation;
-    SpriteRenderer sr;
+    private SpriteRenderer sr;
+    private bool canMove = true; // new variable to control movement
+    private bool isIdling = false; // new variable to check if IdleAnim coroutine is running
+
     // Start is called before the first frame update
     void Start()
     {
         animation = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
+        StartCoroutine(WaitRandom());
     }
 
     // Update is called once per frame
     void Update()
     {
-       // animation.SetFloat("xSpeed", 2);
-        if (rightmove == true)
+        if (canMove && !isIdling) // check if enemy can move and if IdleAnim is not running
         {
-            transform.position += new Vector3(2f, 0, 0) * Time.deltaTime;
-            animation.SetFloat("xSpeed", 2);
-            sr.flipX = false;
-        }
-        else
-        {
-            transform.position += new Vector3(-2f, 0, 0) * Time.deltaTime;
-            animation.SetFloat("xSpeed", -2);
-            sr.flipX = true;
+            if (rightmove == true)
+            {
+                transform.position += new Vector3(2f, 0, 0) * Time.deltaTime;
+                animation.SetFloat("xSpeed", 2);
+                sr.flipX = false;
+            }
+            else
+            {
+                transform.position += new Vector3(-2f, 0, 0) * Time.deltaTime;
+                animation.SetFloat("xSpeed", -2);
+                sr.flipX = true;
+            }
         }
     }
 
@@ -47,8 +53,21 @@ public class EnemyMove : MonoBehaviour
 
     IEnumerator IdleAnim()
     {
-        yield return new WaitForSeconds(1f);
-
+        isIdling = true; // set isIdling to true before starting the coroutine
+        canMove = false; // set canMove to false before starting the coroutine
+        animation.SetBool("Idle", true);
+        yield return new WaitForSeconds(1.5f);
+        animation.SetBool("Idle", false);
+        canMove = true; // set canMove to true when the coroutine is finished
+        isIdling = false; // set isIdling to false when the coroutine is finished
     }
-
+    IEnumerator WaitRandom()
+    {
+        float waitTime = Random.Range(3f, 5f);
+        yield return new WaitForSeconds(waitTime);
+        Debug.Log("Coroutine finished after waiting for " + waitTime + " seconds.");
+        StartCoroutine(IdleAnim());
+        yield return new WaitForSeconds(2f); // wait 2 seconds before WaitRandom 
+        StartCoroutine(WaitRandom());
+    }
 }
