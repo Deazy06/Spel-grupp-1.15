b1f2449f3d98021c114919ae2f42d256976090b8
Deazy06 <120008020+Deazy06@users.noreply.github.com>
2023-01-20 14:20:10 +0100

    bbuilddd

--- Assets/Scripts/Cutscene/ChangeToGame.cs
+++ Assets/Scripts/Cutscene/ChangeToGame.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ChangeToGame : MonoBehaviour
 {
+    public GameObject Cutscene1;
+    public GameObject Cutscene2;
+    public GameObject Cutscene3;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,14 @@ public class ChangeToGame : MonoBehaviour
     IEnumerator Game()
     {
         yield return new WaitForSeconds(5.875f);
+        Cutscene1.SetActive(false);
+        Cutscene2.SetActive(true);
+
+        yield return new WaitForSeconds(1.750f);
+        Cutscene2.SetActive(false);
+        Cutscene3.SetActive(true);
+
+        yield return new WaitForSeconds(3.840f);
         SceneManager.LoadScene("NoelMain");
     }
 }
