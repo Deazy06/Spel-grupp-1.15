785ae090c302db9ac3e40e68d76f1eafadc693e9
SoggyFloorBiscut <120007739+SoggyFloorBiscut@users.noreply.github.com>
2023-01-20 12:41:07 +0100

    Hejdå diyor dörrar som inte funkar

--- Assets/Scripts/World/DoorSetActive.cs
+++ Assets/Scripts/World/DoorSetActive.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 public class DoorSetActive : MonoBehaviour
 {
     private Animator animation;
+    //private SpriteRenderer sprite;
 
     public void Start()
     {
-        animation = GetComponent<Animator>();
+        //sprite = GetComponent<SpriteRenderer>();
+        //animation = GetComponent<Animator>();
     }
 
     //public AudioSource door;
     public void OpenDoor()
     {
+<<<<<<< Updated upstream
 
+=======
+        //GetComponent<Collider2D>().enabled = false;
+        //animation.SetTrigger("DoorOpen");
+        //sprite.color = new Color(0, 0, 0, 0);
+        
+>>>>>>> Stashed changes
         gameObject.SetActive(false);
         //door.Play();
     }
@@ -20,8 +29,22 @@ public class DoorSetActive : MonoBehaviour
 
     public void CloseDoor()
     {
+<<<<<<< Updated upstream
 
         gameObject.SetActive(true);
+=======
+        gameObject.SetActive(true);
+        GetComponent<Collider2D>().enabled = true;
+
+        if (GetComponent<Collider2D>().enabled == false)
+        {
+            //animation.SetTrigger("DoorClose");
+            //sprite.color = new Color(1, 1, 1, 1);
+
+
+        }
+        
+>>>>>>> Stashed changes
     }
 }
     
