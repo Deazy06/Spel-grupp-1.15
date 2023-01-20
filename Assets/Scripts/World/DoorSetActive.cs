657b4d6f0c7ef170720338ea730f00db462c42b0
Deazy06 <120008020+Deazy06@users.noreply.github.com>
2023-01-20 12:54:49 +0100

    eee

--- Assets/Scripts/World/DoorSetActive.cs
+++ Assets/Scripts/World/DoorSetActive.cs
@@ -2,51 +2,39 @@ using UnityEngine;
 
 public class DoorSetActive : MonoBehaviour
 {
+    public int open;
     private Animator animation;
-    //private SpriteRenderer sprite;
 
     public void Start()
     {
-        //sprite = GetComponent<SpriteRenderer>();
-        //animation = GetComponent<Animator>();
+        animation = GetComponent<Animator>();
     }
 
     //public AudioSource door;
     public void OpenDoor()
     {
-<<<<<<< Updated upstream
-
-=======
-        //GetComponent<Collider2D>().enabled = false;
-        //animation.SetTrigger("DoorOpen");
-        //sprite.color = new Color(0, 0, 0, 0);
-        
->>>>>>> Stashed changes
-        gameObject.SetActive(false);
+        if (open == 1)
+        {
+            GetComponent<Collider2D>().enabled = false;
+        }
+        if (open == 3)
+        {
+            open = 2;
+        }
+        open += 1;
+        animation.SetTrigger("DoorOpen");
+        //gameObject.SetActive(false);
         //door.Play();
     }
 
 
     public void CloseDoor()
     {
-<<<<<<< Updated upstream
-
-        gameObject.SetActive(true);
-=======
-        gameObject.SetActive(true);
         GetComponent<Collider2D>().enabled = true;
-
-        if (GetComponent<Collider2D>().enabled == false)
-        {
-            //animation.SetTrigger("DoorClose");
-            //sprite.color = new Color(1, 1, 1, 1);
-
-
-        }
-        
->>>>>>> Stashed changes
+        animation.SetTrigger("DoorClose");
+        //gameObject.SetActive(true);
     }
 }
-    
-   
-    
+
+
+
