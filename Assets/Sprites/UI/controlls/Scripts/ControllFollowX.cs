0c20ac1b2bfc0b6d3384b08738d172fc20f3b6d5
Deazy06 <120008020+Deazy06@users.noreply.github.com>
2023-02-22 09:44:01 +0100

    Update ControllFollowX.cs

--- Assets/Sprites/UI/controlls/Scripts/ControllFollowX.cs
+++ Assets/Sprites/UI/controlls/Scripts/ControllFollowX.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
-public class ControllFollowX : MonoBehaviour
+public class ControllFollowX : MonoBehaviour // OnScreenControlls följer player på X-axeln / Diyor
 {
     private Vector3 offset = new Vector3(-1f, 2.85f, 0f);
-    public float smoothTime = 0.1f;
-    private Vector3 velocity = Vector3.zero;
     [SerializeField] private Transform player;
 
     void Start()
@@ -21,10 +19,5 @@ public class ControllFollowX : MonoBehaviour
 
         Vector3 pos = new Vector3(player.position.x, transform.position.y, transform.position.z);
         transform.position = pos ;
-
-        /*
-        Vector3 targetPosition = player.position + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-        */
     }
 }
